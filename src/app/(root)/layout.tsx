f0daa3b052adb2acb18ae03b4e1ac564153f0231
Rody Huancas <rodyhuancas.04@gmail.com>
2025-05-23 04:45:51 -0500

    Feat: Update layout components to improve structure and styling

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,10 +2,10 @@ import { Header } from "@/components/common/Header";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="h-full overflow-y-auto">
       <Header />
       <main className="pt-40 pb-10 text-foreground w-[80%] mx-auto">{children}</main>
-    </>
+    </div>
   );
 };
 
