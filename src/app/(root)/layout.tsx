8583bb2f60db825c24c30515a4fc41532c1d3b44
Rody Huancas <rodyhuancas.04@gmail.com>
2025-05-25 01:29:39 -0500

    Feat: Update layout and HomePage components for improved structure and styling

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,7 +4,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full overflow-y-auto">
       <Header />
-      <main className="pt-40 pb-10 text-foreground w-[80%] mx-auto">{children}</main>
+      <main className="pt-40 pb-10 text-foreground w-[90%] md:w-[80%] mx-auto">{children}</main>
     </div>
   );
 };
